20bf83dd4e001a71de53d750a8234bdc854d96b7
BookRodrigo <rodrigojoelhepp@gmail.com>
2024-07-27 11:03:12 -0300

    ajustada a tela de login e padronizadas as cores

--- src/main/java/br/edu/unoesc/config/SecurityConfig.java
+++ src/main/java/br/edu/unoesc/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.authorizeHttpRequests((requests) -> requests
-						.requestMatchers("/", "/css/pages/login.css").permitAll()
+						.requestMatchers("/", "/css/pages/login.css", "/css/pages/cores.css").permitAll()
 						.anyRequest().authenticated())
 						.formLogin(formLogin ->
 						formLogin
